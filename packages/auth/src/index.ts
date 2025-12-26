1248d1117a23860fa77468e3d6a5f30e4a9af751
novinbukannopin <jylovien@gmail.com>
2025-12-26 18:32:58 +0700

    feat: initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,24 @@
+import { db } from "@better-auth-admin/db";
+import * as schema from "@better-auth-admin/db/schema/auth";
+import { env } from "@better-auth-admin/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+});
