309e7186585ce0dd5509ceef3f79b1a9f1f57222
novinbukannopin <jylovien@gmail.com>
2026-02-07 15:27:37 +0700

    feat: split global CSS into separate files for app and internal layouts

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 
 import { Geist, Geist_Mono } from "next/font/google";
 
-import "../../index.css";
+import "../../globals-app.css";
 import Providers from "@/components/providers";
 
 const geistSans = Geist({
